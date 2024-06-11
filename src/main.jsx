b05f3bf81a21a2073efcdb316f36b414832e186d
IVJAlberto <ivjalberto@gmail.com>
2024-06-11 09:26:06 -0600

    Agregando pantalla loading al suspense en main.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import ReactDOM from 'react-dom/client'
+import { Suspense } from 'react'
 
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import { BoxColliderWeb } from './BoxColliderWeb'
+import './i18n/i18n'
 import './style.css'
+import { Loading } from './components/Loading'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-      <BoxColliderWeb/>  
-  </BrowserRouter>,
-)
+  <Suspense fallback={<Loading/>}>
+    <BrowserRouter>
+        <BoxColliderWeb/>  
+    </BrowserRouter>
+  </Suspense>
+  )    
