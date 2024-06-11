05afe45dbc1eddf8be512b74a740bfacd97a4415
IVJAlberto <ivjalberto@gmail.com>
2024-06-11 09:26:21 -0600

    Iniciando diseño de componente Services.

---
+++ src/components/Services.jsx
@@ -0,0 +1,24 @@
+
+export const Services = () => {
+  return (
+    <div>   
+        <section className="w-full h-[800px] bg-primary grid grid-rows-3">
+            <div className="grid grid-cols-2">
+                <div>
+                    <img className='max-w-7xl ' src='/assets/servicios/rectangulo.svg' alt="" />
+                </div>
+                <div>
+                    <img className='max-w-7xl  ' src='/assets/servicios/solutions.svg' alt="" />
+                </div>
+            </div>
+            <div>
+                <img className='max-w-7xl  ' src='/assets/servicios/foryou.svg' alt="" />
+            </div>
+            <div>
+            Fusionamos tecnología y creatividad para dar vida a tus visiones digitales. Desde aplicaciones móviles hasta experiencias de realidad aumentada, estamos aquí para llevar tus ideas al siguiente nivel.
+            </div>
+
+        </section>
+    </div>
+  )
+}
