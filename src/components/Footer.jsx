13e32daee7947534e68f8c09a7a851a962e25823
IVJAlberto <ivjalberto@gmail.com>
2024-06-22 13:54:21 -0600

    Editando el Footer para hacerlo responsive.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,49 @@
 
 export const Footer = () => {
   return (
-    <div className="h-[50vh] bg-[#414141] grid grid-cols-1 grid-rows-5">
+    <div className="h-[60vh] md:h-[50vh] bg-[#414141] grid grid-cols-1 grid-rows-11 md:grid-rows-7">
 
-        <section className="row-span-1 flex-wrap content-center flex justify-center">
+        <section className="row-span-3 flex-wrap content-center flex justify-center">
             <img className="h-16" src="/assets/footer/isotipowhite.svg" alt="" />
         </section>
 
-        <section className="row-span-1 flex-wrap content-center grid justify-center grid-rows-2">
-            <div className="grid grid-rows-2">
-                <h1 className="text-white text-xl font-montserratregular">Síguenos</h1>
-                <div className="grid grid-cols-3 gap-4">
-                    <img src="/assets/footer/facebook.svg" alt="" />
-                    <img src="/assets/footer/instagram.svg" alt="" />
-                    <img src="/assets/footer/linkedin.svg" alt="" />
+        <section className="row-span-6 md:row-span-2 grid grid-rows-6 grid-cols-1 md:grid-rows-1 md:grid-cols-6 justify-center md:px-[10vw]">
+
+            <div className="row-span-2 md:col-span-2 md:row-span-1 grid grid-rows-2 md:grid-rows-3 flex-wrap content-center justify-center pt-2 md:pt-0">
+                <h1 className="flex-wrap content-center md:content-start text-white text-xl text-center font-montserratregular">Síguenos</h1>
+                <div className="flex justify-center gap-6 flex-wrap md:content-start">
+                    <a href="https://www.facebook.com/0MiedoDevs" target="_blank">
+                        <img className="h-6" src="/assets/footer/facebook.svg" alt="" />    
+                    </a>
+                    <a href="https://www.instagram.com/0MiedoDevs/" target="_blank">
+                        <img className="h-6"src="/assets/footer/instagram.svg" alt="" />
+                    </a>
+                    <a href="https://mx.linkedin.com/company/boxcollider" target="_blank">
+                        <img className="h-6"src="/assets/footer/linkedin.svg" alt="" />
+                    </a>
                 </div>
             </div>
-        </section>
-        
-        <section className="row-span-1">
-            <h1 className="text-white text-center text-xl font-montserratregular">Morelia, Michoacán, México</h1>
-        </section>
-
-        <section className="row-span-1 flex-wrap content-center grid justify-center grid-rows-2 bg-slate-900">
-            <h1 className="text-white text-xl text-center font-montserratregular">Contáctanos</h1>
-            <div className="grid grid-cols-2">
-                <img className="col-span-1 bg-black h-[28px]" src="/assets/footer/email.svg" alt="" />
-                <img className="col-span-1 h-[28px]" src="/assets/footer/whatsapp.svg" alt="" />
+            
+            <div className=" row-span-2 md:col-span-2 flex-wrap content-center md:content-start">
+                <h1 className="text-white text-center text-xl font-montserratregular px-[10vw] md:px-[5vw]">Morelia, Michoacán, México</h1>
             </div>
+
+            <div className="row-span-2 md:col-span-2 md:row-span-1 grid grid-rows-2 md:grid-rows-3 flex-wrap content-center justify-center pt-2 md:pt-0">
+                <h1 className="flex-wrap content-center md:content-start text-white text-xl font-montserratregular">Contáctanos</h1>
+                <div className=" flex justify-center gap-6">
+                    <a href="mailto:contacto@0miedo.com.mx">
+                        <img  className="h-6" src="/assets/footer/email.svg" alt="" />
+                    </a>
+                    <a href="https://api.whatsapp.com/send?phone=5214431819520&amp;text=Hola%2C%20me%20gustar%C3%ADa%20obtener%20una%20cotizaci%C3%B3n." target="_blank">
+                        <img className="h-6" src="/assets/footer/whatsapp.svg" alt="" />
+                    </a>
+                </div>
+            </div>
+
         </section>
 
-        <section className="row-span-1">
-            <h1 className="text-white text-center text-xl font-montserratregular">Morelia, Michoacán, México</h1>
+        <section className="row-span-2 flex-wrap content-center flex justify-center px-[30vw]">
+            <h1 className="text-white text-center text-sm font-montserratregular"> Hecho con ❤️ por BoxCollider 2024</h1>
         </section>
 
     </div>
