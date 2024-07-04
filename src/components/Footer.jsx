2f83973fe3d328654c40e3faa06dba0be3a27f1c
Beto <ivjalberto@gmail.com>
2024-07-04 16:17:36 -0600

    Corrigiendo redes.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,10 +25,10 @@ export const Footer = () => {
                 <div className="row-span-2 md:col-span-2 md:row-span-1 grid grid-rows-2 md:grid-rows-3 flex-wrap content-center justify-center pt-2 md:pt-0">
                     <h1 className="flex-wrap content-center md:content-start text-white text-xl text-center font-montserratregular">{t('footer01')}</h1>
                     <div className="flex justify-center gap-6 flex-wrap md:content-start pt-4 md:pt-0">
-                        <a href="https://www.facebook.com/0MiedoDevs" target="_blank">
+                        <a href="https://www.facebook.com/BoxColliderDevs" target="_blank">
                             <img className="h-6" src="/assets/footer/facebook.svg" alt="" />    
                         </a>
-                        <a href="https://www.instagram.com/0MiedoDevs/" target="_blank">
+                        <a href="https://www.instagram.com/BoxColliderDevs/" target="_blank">
                             <img className="h-6"src="/assets/footer/instagram.svg" alt="" />
                         </a>
                         <a href="https://mx.linkedin.com/company/boxcollider" target="_blank">
@@ -44,7 +44,7 @@ export const Footer = () => {
                 <div className="row-span-2 md:col-span-2 md:row-span-1 grid grid-rows-2 md:grid-rows-3 flex-wrap content-center justify-center pt-2 md:pt-0">
                     <h1 className="flex-wrap content-center md:content-start text-white text-xl font-montserratregular">{t('footer02')}</h1>
                     <div className=" flex justify-center gap-6 pt-4 md:pt-0">
-                        <a href="mailto:contacto@0miedo.com.mx">
+                        <a href="mailto:contacto@boxcollider.com.mx">
                             <img  className="h-6" src="/assets/footer/email.svg" alt="" />
                         </a>
                         <a href="https://api.whatsapp.com/send?phone=5214431819520&amp;text=Hola%2C%20me%20gustar%C3%ADa%20obtener%20una%20cotizaci%C3%B3n." target="_blank">
