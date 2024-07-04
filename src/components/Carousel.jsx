4318d9897feb60ed35f12a44cf6ab53affd9c34d
IVJAlberto <ivjalberto@gmail.com>
2024-07-04 10:52:48 -0600

    Cambiando imagenes del carrusel.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -37,7 +37,7 @@ const Carousel = () => {
     return (
       <section id="idPortafolio" className={` ${height>=800 ? ' pt-24' : 'pt-0'} h-[100vh]  ss:px-2 xs:px-12 sm:px-32 md:px-0 flex-wrap content-center flex justify-center`}>
         <div className="h-[90%] w-[100%] grid grid-rows-10 grid-cols-1 md:grid-rows-1 md:grid-cols-10">
-          <div className=" h-[100%] row-span-1 md:col-span-1 flex-wrap content-end md:content-center flex justify-between md:justify-end px-10 md:px-0 translate-y-14 z-20 md:translate-y-0">
+          <div className="z-40 h-[100%] row-span-1 md:col-span-1 flex-wrap content-end md:content-center flex justify-between md:justify-end px-10 md:px-0 translate-y-14 md:translate-y-0">
             <Button disableRipple style={{ backgroundColor: 'transparent' }}>
               <img className="h-[36px] md:h-[50px] invisible md:visible bg-black rounded-full p-1" onClick={handlePrev} src="/assets/carousel/leftarrow.svg" alt="" />
             </Button>
@@ -67,15 +67,15 @@ const Carousel = () => {
               {
                   productos.map(slide => (
                       <SwiperSlide key={slide.k}> 
-                          <div className={`h-full w-full px-8 md:p-8`}>
+                          <div className={`h-full z-50 w-full mt-8 px-8 md:p-8`}>
                             <div className="h-[10%] flex-wrap content-center">
-                              <h1 className="text-xl md:text-2xl font-montserratregular text-azul-0m text-center">{slide.title}</h1>
+                              {/* <h1 className="text-xl md:text-2xl font-montserratregular text-azul-0m text-center">{slide.title}</h1> */}
+                              <h1 className="text-base font-montserratregular text-azul-0m text-center">{slide.tag}</h1>
                             </div>
                             <div className="h-[80%]">
                               <img className="h-full w-full flex justify-center object-cover rounded-2xl" src={`${slide.img}`} alt={`${slide.title}`} />
                             </div>
-                            <div className="h-[5%] flex-wrap content-center">
-                              <h1 className="text-base font-montserratregular text-azul-0m text-center">{slide.tag}</h1>
+                            <div className="h-[10%] flex-wrap content-center">
                             </div>
                           </div>
                       </SwiperSlide>
@@ -84,7 +84,7 @@ const Carousel = () => {
               } 
           <div className="swiper-pagination visible md:invisible"></div>
           </Swiper>
-          <div className="h-[0%] md:h-[100%] row-span-0 col-span-1 flex-wrap content-center flex justify-start">
+          <div className="z-40 h-[0%] md:h-[100%] row-span-0 col-span-1 flex-wrap content-center flex justify-start">
             <Button disableRipple style={{ backgroundColor: 'transparent' }}>
               <img className="h-[50px] bg-black rounded-full p-1 invisible md:visible" onClick={handleNext} src="/assets/carousel/rightarrow.svg" alt="" />
             </Button>
